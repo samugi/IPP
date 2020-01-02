460929a5d2a45944f642a0bb24badae289895275
Snorlax91 <38894491+Snorlax91@users.noreply.github.com>
2020-01-02 22:45:46 +0100

    Update autoScript.py

--- autoScript.py
+++ autoScript.py
@@ -3,8 +3,10 @@ import win32gui
 import win32con
 import win32api
 import random
+import keyboard
 import time
 from time import sleep
+from Comment import Comment
 
 def is_win_ok(hwnd, starttext):
     s = win32gui.GetWindowText(hwnd)
@@ -41,18 +43,113 @@ def main():
     print(hwnd)
     return hwnd
 	
+def convertCommandInEnum(command):
+	if "up" in command:
+		return 3
+	elif "su" in command:
+		return 3
+	elif "avanti" in command:
+		return 3
+	elif "down" in command:
+		return 4
+	elif "giù" in command:
+		return 4
+	elif "giu" in command:
+		return 4
+	elif "a" in command:
+		return 1
+	elif "ok" in command:
+		return 1
+	elif "b" in command:
+		return 2
+	elif "no" in command:
+		return 2
+	elif "left" in command:
+		return 5
+	elif "sinistra" in command:
+		return 5
+	elif "sx" in command:
+		return 5
+	elif "lf" in command:
+		return 5
+	elif "right" in command:
+		return 6 
+	elif "destra" in command:
+		return 6 
+	elif "dx" in command:
+		return 6
+	elif "rt" in command:
+		return 6
+	elif "start" in command:
+		return 7
+	elif "menu" in command:
+		return 7
+	elif "pause" in command:
+		return 7
+	elif "pausa" in command:
+		return 7
+	elif "p" in command:
+		return 7
 
-
+def sendCommand(command):
+	command = convertCommandInEnum(command)
+	if command == 1:	
+		#press A
+		keyboard.press('a')
+		sleep(.250)
+		keyboard.release('a')
+		print("Button pressed: A")
+	elif command == 2:
+		#press Z
+		keyboard.press('z')
+		sleep(.250)
+		keyboard.release('z')
+		print("Button pressed: B")
+	elif command == 3:
+		#press I (UP)
+		keyboard.press('i')
+		sleep(.250)
+		keyboard.release('i')
+		print("Button pressed: Arrow UP")
+	elif command == 4:
+		#press K (DOWN)
+		keyboard.press('k')
+		sleep(.250)
+		keyboard.release('k')
+		print("Button pressed: Arrow DOWN")
+	elif command == 5:
+		#press J (LEFT)
+		keyboard.press('j')
+		sleep(.250)
+		keyboard.release('j')
+		print("Button pressed: Arrow LEFT")
+	elif command == 6:
+		#press L (RIGHT)
+		keyboard.press('l')
+		sleep(.250)
+		keyboard.release('l')
+		print("Button pressed: Arrow RIGHT")
+	elif command == 7:
+		#press P (start)
+		keyboard.press('p')
+		sleep(.250)
+		keyboard.release('p')
+		print("Button pressed: START")
+		
+def sendCommands(command, n):
+	for x in range(1, n):
+		sendCommand(command)
 #[hwnd] No matter what people tell you, this is the handle meaning unique ID, 
 #["Notepad"] This is the application main/parent name, an easy way to check for examples is in Task Manager
 #["test - Notepad"] This is the application sub/child name, an easy way to check for examples is in Task Manager clicking dropdown arrow
 #hwndMain = win32gui.FindWindow("Notepad", "test - Notepad") this returns the main/parent Unique ID
-hwndMain = main() #win32gui.FindWindow("VisualBoyAdvance emulator (32 bit)", "VisualBoyAdvance")
+hwndMain = main()#win32gui.FindWindow("Notepad", "Untitled - Notepad")
 print(hwndMain) #you can use this to see main/parent Unique ID
 #["hwndMain"] this is the main/parent Unique ID used to get the sub/child Unique ID
 #[win32con.GW_CHILD] I havent tested it full, but this DOES get a sub/child Unique ID, if there are multiple you'd have too loop through it, or look for other documention, or i may edit this at some point ;)
 #hwndChild = win32gui.GetWindow(hwndMain, win32con.GW_CHILD) this returns the sub/child Unique ID
-hwndChild = hwndMain
+hwndChild =  hwndMain #win32gui.GetWindow(hwndMain, win32con.GW_CHILD)
+win32gui.SetForegroundWindow(hwndChild)
 print(hwndChild)  #you can use this to see sub/child Unique ID
 
 #While(True) Will always run and continue to run indefinitely
@@ -62,53 +159,25 @@ while(True):
 	#[0x44] hex code for D
 	#[0]No clue, good luck!
 	#temp = win32api.PostMessage(hwndChild, win32con.WM_CHAR, 0x44, 0) returns key sent
-	r = random.randint(1,7)
 	
-	if r == 1:	
-		#press A
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x41, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x41, 0)
-		print("Ho premuto A")
-	elif r == 2:
-		#press Z
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x5A, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x5A, 0)
-		print("Ho premuto Z")
-	elif r == 3:
-		#press I (UP)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x49, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x49, 0)
-		print("Ho premuto I")
-	elif r == 4:
-		#press K (DOWN)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x4B, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x4B, 0)
-		print("Ho premuto K")
-	elif r == 5:
-		#press J (LEFT)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x4A, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x4A, 0)
-		print("Ho premuto J")
-	elif r == 6:
-		#press L (RIGHT)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x4C, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x4C, 0)
-		print("Ho premuto L")
-	elif r == 7:	
-		#press P (start)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYDOWN, 0x50, 0)
-		time.sleep(1)
-		temp = win32api.PostMessage(hwndChild, win32con.WM_KEYUP, 0x50, 0)
-		print("Ho premuto P")
-
-
+	commentsArray = ["up", "down", "avanti", "destra", "ok", "no"]
+	
+	for comment in commentsArray:
+		words = comment["text"].split(" ")
+		print(words[0])
+		if len(words) > 2:
+			print("More then one words")
+			continue
+		elif len(words) == 2:
+			print("Two words")
+			if words[0].isnumeric():
+				if words[0] <= 10:
+					sendCommands(words[1],words[0])
+				continue
+			continue
+		elif len(words) == 1:
+			print("One word")
+			sendCommand(words[0])
 	#print(temp) prints the returned value of temp, into the console
-	print(temp)
 	#sleep(1) this waits 1 second before looping through again
 	sleep(1)
