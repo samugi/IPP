33ed51f3019fca00696be79c6671c63786df029c
root <root@instance-ipp1.us-central1-a.c.hopeful-keep-264017.internal>
2020-01-03 21:09:05 -0500

    quel cazzo che ti apre

--- script.sh
+++ script.sh
@@ -1,1 +1,3 @@
-vba ~/Programs/IPP/Pokemon\ Red\ \(UE\)\ \[S\]\[\!\].gb --throttle=100
+vba ~/Programs/IPP/Pokemon\ Red\ \(UE\)\ \[S\]\[\!\].gb --throttle=100 &
+python3 ~/Programs/IPP/ipp.py &
+wmctrl -a VisualBoyAdvance
