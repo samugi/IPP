4689d64133e42ae6fc97a604e5cfd46ad74d99ef
samu91gn <samuele8@gmail.com>
2020-02-20 22:27:16 +0100

    launcher script

---
+++ launcher.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM start the backend application that receives the commands from the server and emits the key presses to the emulators
+ECHO starting app.js
+start cmd.exe /c node Backend\command-log\app.js
+REM start the Server that is going to listen for the incoming commands from the platforms
+ECHO starting server
+start cmd.exe /c node Backend\node-service\server.js
